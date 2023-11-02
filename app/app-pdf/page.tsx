37a9b6fdb22bc0fd65c607626cad75d017473c53
Marc-Andre Quintal <maquintal16@gmail.com>
2023-11-01 22:36:08 -0400

    ca marche, mais ca manque de finesse

--- app/app-pdf/page.tsx
+++ app/app-pdf/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 
 import React, { useState } from 'react';
-import { PDFDocument, rgb } from 'pdf-lib';
+import { PDFDocument, PDFForm, rgb } from 'pdf-lib';
 import InputAutoComplete from '@/src/inputs/AutoComplete';
 import { useForm } from "react-hook-form";
+import { Button } from '@mui/material';
 
-const buildDynamicForm = (form, control) => {
+const buildDynamicForm = (form: PDFForm, control: any) => {
 
   if (form) {
-    console.log(form)
-
-    // Set form field values from the form data
-    // form.getTextField('Name').setText(formData.name);
+    // console.log(form)
 
     const fields = form.getFields();
 
@@ -30,10 +28,8 @@ const buildDynamicForm = (form, control) => {
         return (
           <InputAutoComplete
             control={control}
-            name="test"
-            label="autocomplete"
-            // name={`scenarios.${index}.loanDurationInYears`}
-            // label={`${intl.formatMessage({ id: "calculators.form.sectionLoanInfo.inputs.loanDurationInYears" })}`}
+            name={`${field.getName()}`}
+            label={`${field.getName()}`}
             rules={{ required: true }}
             options={opt}
           />
@@ -41,49 +37,39 @@ const buildDynamicForm = (form, control) => {
         // return (form.getDropdown(field.getName()).getOptions())
       }
 
-
     }
   }
 
-
-
-  // Create a new PDF with the updated form fields
-  // const pdfBytes = await pdfDoc.save();
-
-  // // Save or display the modified PDF
-  // // For simplicity, you can just display it in a new tab here
-  // const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-  // const url = window.URL.createObjectURL(blob);
-  // window.open(url);
 };
 
 
 
-const loadPdf = async (setForm) => {
+const loadPdf = async (setForm: any, setPdfDoc: any) => {
 
   // Load an existing PDF with editable fields
   const existingPdfBytes = await fetch('existing.pdf').then((res) => res.arrayBuffer());
   const pdfDoc = await PDFDocument.load(existingPdfBytes);
   const form = pdfDoc.getForm();
   // console.log(form)
+  // console.log(pdfDoc)
 
   // return form
   setForm(form)
+  setPdfDoc(pdfDoc)
 
 }
 
 function App() {
 
   const { handleSubmit, control, getValues, formState, setValue, setError } = useForm({
-    defaultValues: {
-      test: ""
-    },
+    defaultValues: {},
     mode: "onSubmit"
   });
 
   const [state, setState] = useState([])
   const [form, setForm] = useState()
-  // const [formData, setFormData] = useState({ name: '' });
+  const [pdfDoc, setPdfDoc] = useState()
+  const [formData, setFormData] = useState({});
 
 
   // const handleInputChange = (e) => {
@@ -92,21 +78,72 @@ function App() {
   // };
 
   React.useEffect(() => {
-    loadPdf(setForm)
+    loadPdf(setForm, setPdfDoc)
   }, [])
 
   React.useEffect(() => {
     // console.log(form)
+    // @ts-ignore
     setState(buildDynamicForm(form, control))
   }, [control, form]);
 
-  // const onSubmit = async (data: any) => {
-  //   // console.log(data)
-  // };
+  const test = async () => {
+
+    if (pdfDoc) {
+      // Create a new PDF with the updated form fields
+      const pdfBytes = await pdfDoc.save()
+
+      if (pdfBytes) {
+
+        // Save or display the modified PDF
+        // For simplicity, you can just display it in a new tab here
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        const url = window.URL.createObjectURL(blob);
+        window.open(url);
+      }
+    }
+
+  }
+
+  React.useEffect(() => {
+    console.log(form)
+    console.log(pdfDoc)
+
+    if (Object.keys(formData).length !== 0 && form !== null && form && pdfDoc) {
+      // Set form field values from the form data
+      Object.entries(formData).forEach(([key, value]) => {
+        // form.getTextField(key).setValue(value) // .setText(value);
+        // form.getDropdown(key).select(value)
+        const dropdownField = form.getField(key);
+        dropdownField.select(value);
+      });
+
+      test()
+
+    }
+
+  }, [formData, form, pdfDoc])
+
+  const onSubmit = async (data: any/* , form: any, pdfDoc: any */) => {
+    setFormData(data)
+
+    // const data = await getValues()
+    // console.log(data)
+
+  };
 
   return (
     <>
-      {state}
+      <form>
+        {/* {console.log(form)} */}
+        {state}
+        <Button
+          // onClick={handleSubmit(onSubmit(form, pdfDoc))}
+          onClick={handleSubmit(onSubmit)}
+        > submit
+        </Button>
+        {JSON.stringify(formData)}
+      </form>
     </>
     // <div>
     //   <label>
